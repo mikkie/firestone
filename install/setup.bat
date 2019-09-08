4907287937be395d8a8a3ded762e9f8cef430010
mikkie <lmj3648xm@hotmail.com>
2019-09-08 20:44:22 +0800

    remove unuse files

--- install/setup.bat
+++
@@ -1,1 +0,0 @@
-pip3 install -r requirements.txt --trusted-host pypi.python.org --trusted-host pypi.org --trusted-host files.pythonhosted.org
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <header>
-      <span>Vue.js PWA</span>
+      <span>Firestone</span>
     </header>
     <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
+      <img src="./assets/logo.png" alt="Firestone">
       <router-view></router-view>
     </main>
   </div>
